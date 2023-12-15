cf6048bb01b76ae62f175a0df627f5e43e34aa8c
Yael Duckwen <yaelduckwen@gmail.com>
2023-12-15 20:11:12 -0300

    componentes para logos

---
+++ components/FranchiseItem.vue
@@ -0,0 +1,19 @@
+<script setup>
+	const props = defineProps({
+		id: String,
+		title: String,
+	});
+</script>
+
+<template>
+	<NuxtLink :to="'/franchises/' + id">
+		<article class="uk-card uk-card-small uk-text-center">
+			<div class="uk-card-media-top">
+				<img src="https://getuikit.com/docs/images/light.jpg" width="1800" height="1200" alt="">
+			</div>
+			<div class="uk-card-body">
+				<h3 class="uk-card-title">{{title}}</h3>
+			</div>
+		</article>
+	</NuxtLink>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -16,38 +16,20 @@
 	<h1>Wellcome to JP Collections</h1>
 	<section class="uk-section uk-section-small">
 		<h2>Brands <NuxtLink to="/brands" class="uk-button uk-button-primary uk-button-small">See all</NuxtLink></h2>
-		<div class="uk-grid uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m uk-child-width-1-5@l" uk-grid uk-height-match="target: .uk-card">
-			<div v-for="brand in brands" :key="brand.id">
-				<NuxtLink :to="'/brands/' + brand.id">
-					<div class="uk-card uk-card-default uk-card-small">
-						<div class="uk-card-media-top">
-							<img src="https://getuikit.com/docs/images/light.jpg" width="1800" height="1200" alt="">
-						</div>
-						<div class="uk-card-body">
-							<h3 class="uk-card-title">{{brand.title}}</h3>
-						</div>
-					</div>
-				</NuxtLink>
-			</div>
-		</div>
+		<ul class="uk-grid uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m uk-child-width-1-5@l" uk-grid uk-height-match="target: .uk-card">
+			<li v-for="brand in brands" :key="brand.id">
+				<BrandItem :id="brand.id" :title="brand.title" />
+			</li>
+		</ul>
 	</section>
 
 	<section class="uk-section uk-section-small">
 		<h2>Franchises <NuxtLink to="/franchises" class="uk-button uk-button-primary uk-button-small">See all</NuxtLink></h2>
-		<div class="uk-grid uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m uk-child-width-1-5@l" uk-grid uk-height-match="target: .uk-card">
-			<div v-for="franchise in franchises" :key="franchise.id">
-				<NuxtLink :to="'/franchises/' + franchise.id">
-					<div class="uk-card uk-card-default uk-card-small">
-						<div class="uk-card-media-top">
-							<img src="https://getuikit.com/docs/images/light.jpg" width="1800" height="1200" alt="">
-						</div>
-						<div class="uk-card-body">
-							<h3 class="uk-card-title">{{franchise.title}}</h3>
-						</div>
-					</div>
-				</NuxtLink>
-			</div>
-		</div>
+		<ul class="uk-grid uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m uk-child-width-1-5@l" uk-grid uk-height-match="target: .uk-card">
+			<li v-for="franchise in franchises" :key="franchise.id">
+				<FranchiseItem :id="franchise.id" :title="franchise.title" />
+			</li>
+		</ul>
 	</section>
 	
 </template>
